fcd2f31de64930f3332d9c95c2de5107846a034f
ponurakk <p0unrakg@int.pl>
2023-04-24 18:53:40 +0200

    Create wallet and economy with user

--- src/controllers/users/create.rs
+++ src/controllers/users/create.rs
@@ -2,7 +2,9 @@ use actix_web::{Responder, web, HttpResponse, http::header::ContentType};
 use sea_orm::{Set, EntityTrait};
 use serde_json::json;
 
-use crate::{ApiError, entities::{users, prelude::Users}, AppState};
+use crate::{ApiError, entities::{users, prelude::Users, survival_economy, wallet}, AppState};
+
+use crate::entities::prelude::{Wallet, SurvivalEconomy};
 
 #[utoipa::path(
 	post,
@@ -31,14 +33,46 @@ pub async fn create(
 
 	let user_insert = Users::insert(user).exec(&data.conn).await;
 
-	drop(exotia_key_guard);
-
-	return match user_insert {
-		Ok(v) => {
-			Ok(HttpResponse::Created().finish())
-		},
-		Err(_) => Ok(HttpResponse::Conflict()
+	let user_insert = match user_insert {
+		Ok(v) => v,
+		Err(_) => return Ok(HttpResponse::Conflict()
 			.content_type(ContentType::json())
 			.json(json!({ "message": "User with that uuid already exists" }))),
+	};
+
+	let survival = survival_economy::ActiveModel {
+		user_id: Set(user_insert.last_insert_id),
+		balance: Set(0),
+		..Default::default()
+	};
+
+	let survival_economy_insert = match SurvivalEconomy::insert(survival).exec(&data.conn).await {
+		Ok(v) => v,
+		Err(_) => {
+			Users::delete_by_id(user_insert.last_insert_id);
+
+			return Ok(HttpResponse::InternalServerError()
+				.content_type(ContentType::json())
+				.json(json!({ "message": "Failed to create SurvivalEconomy Table" })));
+		}
+	};
+
+	let wallet_ = wallet::ActiveModel {
+		user_id: Set(user_insert.last_insert_id),
+		coins: Set(0.0),
+		spent_coins: Set(0.0),
+		..Default::default()
+	};
+
+	match Wallet::insert(wallet_).exec(&data.conn).await {
+		Ok(_) => (),
+		Err(_) => {
+			Users::delete_by_id(user_insert.last_insert_id);
+			SurvivalEconomy::delete_by_id(survival_economy_insert.last_insert_id);
+		}
 	}
+
+	drop(exotia_key_guard);
+
+	Ok(HttpResponse::Created().finish())
 }
